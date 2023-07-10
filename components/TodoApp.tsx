c439f1ca34281fe92ada5d9bd7f26d575c20fa70
thomaseng-jp <sophinlavn@gmail.com>
2023-07-10 21:10:52 +0900

    feat: add todo function

--- components/TodoApp.tsx
+++ components/TodoApp.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { getAllTodos } from "../utils/supabaseFunctions";
+import { addTodo, getAllTodos } from "../utils/supabaseFunctions";
 import TodoList from "./TodoList";
 
 const TodoApp = () => {
   const [todos, setTodos] = useState<any>([]);
+  const [title, setTitle] = useState<string>("");
 
   useEffect(() => {
     const getTodos = async () => {
@@ -13,15 +14,34 @@ const TodoApp = () => {
     getTodos();
   }, []);
 
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (title === "") {
+      return;
+    }
+
+    await addTodo(title);
+    let todos = await getAllTodos();
+    setTodos(todos);
+
+    setTitle("");
+  };
+
   return (
     <section className="text-center mb-2 text-2xl font-medium">
       <h3>Supabase Todo App</h3>
-      <form>
-        <input type="text" className="mr-2 shadow-lg p-1 outline-none" />
+      <form onSubmit={e => handleSubmit(e)}>
+        <input
+          type="text"
+          className="mr-2 shadow-lg p-1 outline-none"
+          onChange={e => setTitle(e.target.value)}
+          value={title}
+        />
         <button className="shadow-md border-2 px-1 py-1 rounderd-lg bg-green-200">
           Add
         </button>
-        <TodoList todos={todos} />
+        <TodoList todos={todos} setTodos={setTodos} />
       </form>
     </section>
   );
